cadf601b015c64bb8ed60e0690fa39b8859627fd
UK\Billy.Bellchambers <Billy.bellchambers@theaccessgroup.com>
2018-06-13 14:41:06 +0100

    Add project files.

---
+++ SQLDataSourceDemo/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQLDataSourceDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CountriesSQLDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
